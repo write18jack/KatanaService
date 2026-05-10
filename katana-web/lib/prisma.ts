77f365f6d65ad29c92dde6bce98b2fef6991631a
write18jack <write18jack@gmail.com>
2026-05-11 07:17:38 +0900

    refactor: reorganize prisma.ts

--- katana-web/lib/prisma.ts
+++ katana-web/lib/prisma.ts
@@ -1,18 +1,17 @@
+import "dotenv/config"
 import {PrismaClient } from '@prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
 
 const globalForPrisma = global as unknown as {
-    prisma: PrismaClient
+  prisma: PrismaClient
 }
 
 const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL,
 })
 
-const prisma = globalForPrisma.prisma || new PrismaClient({
+export const prisma =   globalForPrisma.prisma || new PrismaClient({
   adapter,
 })
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
-
-export default prisma
+if (process.env.NODE_ENV !== 'production')   globalForPrisma.prisma = prisma
