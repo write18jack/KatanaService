1fa6c5f8a3eaa63d90d1132cae01434afc7b4e02
write18jack <write18jack@gmail.com>
2026-07-19 09:12:45 +0900

    マスタ作成

---
+++ katana-web/prisma/seeds/appraisers.ts
@@ -0,0 +1,3 @@
+export const appraisers = [
+  { name: "公益財団法人 日本美術刀剣保存協会", displayOrder: 1 },
+];
---
+++ katana-web/prisma/seeds/makers.ts
@@ -0,0 +1,7 @@
+export const makers = [
+  { name: "三条宗近", displayOrder: 1 },
+  { name: "備前長船長光", displayOrder: 2 },
+  { name: "兼定", displayOrder: 3 },
+  { name: "正宗", displayOrder: 4 },
+  { name: "村正", displayOrder: 5 },
+];
